363efb09ab54c7c7533f15eb088197f5148a5ac3
TDPhantom <abattlessmith@brooklynsteamcenter.org>
2022-12-15 10:52:37 -0500

    added

--- main.py
+++ main.py
@@ -2,14 +2,28 @@
 
 file = open("NASA_access_log_Jul95")
 ip_adresses = []
-
+Files = []
+Times = []
+FileType = []
 
 try:
     for line in file:
         ip_adresses.append(line.split(" ")[0])
+        Files.append(line.split(" ")[6])
+        Times.append(line.split(" ")[3].split(":")[0])
+        FileType.append(line.split(" ")[6].split("/")[2])
 except:
         print("uh-oh")
 
-print(ip_adresses)
 
+
+from collections import Counter
+c = Counter(ip_adresses).most_common(1)
+k = Counter(Files).most_common(1)
+t = Counter(Times).most_common(1)
+f = Counter(FileType).most_common(1)
+print(c)
+print(k)
+print(t)
+print(f)
 #
