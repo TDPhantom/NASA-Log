334599822a447e395287afe3f2f9368a9d8f8212
TDPhantom <abattlessmith@brooklynsteamcenter.org>
2022-12-19 10:56:27 -0500

    added

--- practice.py
+++ practice.py
@@ -2,17 +2,16 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 
-xpoints = np.array([3, 8, 1, 10])
-ypoints = np.array([0, 1, 2, 3])
-zpoints = np.array([4, 7 ,9 ,10,25])
+xpoints = np.array([0, 1, 2, 3])
+ypoints = np.array([3, 8, 1, 10])
+x = np.array([0, 1, 2, 3])
+y = np.array([10, 20, 30, 40])
 
 plt.plot(xpoints, ypoints, 'X:r')
 plt.subplot(2, 1, 1)
 
-
-plt.xlabel("Fornite Players")
-plt.ylabel('Minecraft Players')
-
+plt.subplot(2, 1, 2)
+plt.plot(x,y)
 
 
 plt.grid()
